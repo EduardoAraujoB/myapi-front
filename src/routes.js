472f3f6b1269cc559bdec47125fd290c478efec1
unknown <ea0123450@gmail.com>
2019-06-09 12:11:31 -0300

    transforming routes in a stateless component

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -7,37 +7,17 @@ import SignUp from "./pages/SignUp";
 import Articles from "./pages/Articles";
 import Members from "./pages/Members";
 
-class Routes extends Component {
-  this_isMounted = false;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      pages: {
-        Home,
-        Articles,
-        SignIn,
-        SignUp,
-        Members
-      }
-    };
-  }
-
-  render() {
-    const { pages } = this.state;
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={pages.Home} />
-          <Route path="/articles" component={pages.Articles} />
-          <Route path="/signin" component={pages.SignIn} />
-          <Route path="/signup" component={pages.SignUp} />
-          <Route path="/members" component={pages.Members} />
-          <Route exact path="*" component={() => <h1>Page not found</h1>} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/articles" component={Articles} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/members" component={Members} />
+      <Route exact path="*" component={() => <h1>Page not found</h1>} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Routes;
