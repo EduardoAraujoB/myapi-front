10638eba3484f508076a74037fe6075d116ed052
unknown <ea0123450@gmail.com>
2019-06-09 12:40:12 -0300

    added authentication and loading for menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import { Header, Title, MenuButton, Logo } from "./styles";
+import { isAuthenticated } from "../../services/auth";
 
-const Menu = () => {
-  return (
-    <Header>
-      <Logo href="/">Logo</Logo>
-      <Title>Nome do blog</Title>
-      <MenuButton href="/articles">Articles</MenuButton>
-      <MenuButton href="/signup">SignUp</MenuButton>
-      <MenuButton href="/signin">SignIn</MenuButton>
-    </Header>
-  );
-};
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false
+    };
+  }
+  _isMounted = false;
+  loading = false;
+
+  componentWillMount = async () => {
+    this._isMounted = true;
+    this.loading = true;
+    const auth = await isAuthenticated();
+    if (this._isMounted) {
+      this.loading = false;
+      this.setState({ authenticated: auth });
+    }
+  };
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    if (this.loading) {
+      return <h1>Carregando</h1>;
+    }
+    if (this.state.authenticated) {
+      return (
+        <Header>
+          <Logo href="/">Logo</Logo>
+          <Title>Nome do blog</Title>
+          <MenuButton href="/articles">Articles</MenuButton>
+          <MenuButton href="/members">Members</MenuButton>
+          <MenuButton href="/">Logout</MenuButton>
+        </Header>
+      );
+    } else {
+      return (
+        <Header>
+          <Logo href="/">Logo</Logo>
+          <Title>Nome do blog</Title>
+          <MenuButton href="/articles">Articles</MenuButton>
+          <MenuButton href="/signup">SignUp</MenuButton>
+          <MenuButton href="/signin">SignIn</MenuButton>
+        </Header>
+      );
+    }
+  }
+}
 
 export default Menu;
