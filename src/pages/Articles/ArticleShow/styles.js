6bf17f287fe7a1faa50e4fc316653981d70b67ea
unknown <ea0123450@gmail.com>
2019-06-15 12:01:20 -0300

    making layout for comments in page to show an article

--- src/pages/Articles/ArticleShow/styles.js
+++ src/pages/Articles/ArticleShow/styles.js
@@ -4,10 +4,12 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 30px;
+  flex-direction: column;
+  margin: 15px;
 `;
 
 export const Article = styled.div`
+  margin: 15px;
   display: flex;
   flex-direction: column;
   padding: 30px;
@@ -16,20 +18,47 @@ export const Article = styled.div`
   background: #fff;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   border-radius: 20px;
-  strong {
+  & > strong {
     text-align: center;
     font-size: 30px;
     margin-bottom: 30px;
   }
-  p {
+  & > p {
     margin: 30px;
     font-size: 20px;
-    text-align: justify;
   }
-  span {
+  & > span {
     margin: 30px;
     font-size: 20px;
     font-weight: bold;
     text-align: right;
+  }
+`;
+
+export const CommentContainer = styled.div`
+  margin: 15px;
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  width: 100%;
+  height: auto;
+  background: #fff;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 20px;
+  & > strong {
+    text-align: center;
+    font-size: 22px;
+    margin-bottom: 10px;
+  }
+`;
+
+export const Comment = styled.div`
+  margin-top: 10px;
+  width: 100%;
+  height: auto;
+  padding: 10px;
+  border-radius: 10px;
+  & > p {
+    margin: 10px 0 0 10px;
   }
 `;
