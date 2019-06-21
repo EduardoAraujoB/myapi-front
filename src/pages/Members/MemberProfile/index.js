3c061ce393b64495fa6c26aa977a03186ab11c38
unknown <ea0123450@gmail.com>
2019-06-21 18:22:26 -0300

    starting to creating the page to show the profile of the memmber

--- src/pages/Members/MemberProfile/index.js
+++ src/pages/Members/MemberProfile/index.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-// import { Container } from './styles';
+import { withAuthentication } from "../../../components/hocs/Authentication";
+import api from "../../../services/api";
+import Loading from "../../../components/Loading";
+import Menu from "../../../components/Menu";
 
-const MemberProfile = () => {
-  return <h1>Hello World</h1>;
-};
+class MemberProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      member: null
+    };
+  }
+  _isMounted = false;
+  loading = false;
 
-export default MemberProfile;
+  componentWillMount = async () => {
+    this._isMounted = true;
+    this.loading = true;
+    const member = await api.get(`/member`);
+    if (this._isMounted) {
+      this.loading = false;
+      this.setState({ member: member.data });
+    }
+  };
+
+  componentWillUnmount = () => {
+    this._isMounted = false;
+  };
+
+  render() {
+    if (this.props.authentication.loading || this.loading) {
+      return <Loading />;
+    }
+    if (this.props.authentication.authenticated) {
+      if (this.props.authentication.member._id === this.props.match.params.id) {
+        return (
+          <>
+            <Menu auth={this.props.authentication} />
+            <h1>Esse Perfil é seu</h1>
+          </>
+        );
+      } else {
+        return <Redirect to="/" />;
+      }
+    } else {
+      return <Redirect to="/" />;
+    }
+  }
+}
+
+export default withAuthentication()(MemberProfile);
